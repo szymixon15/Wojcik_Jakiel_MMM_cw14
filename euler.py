6f6e327349a739a26d84ba757eca15c508072817
szymixon15 <szymekw1503@gmail.com>
2025-04-22 15:27:28 +0200

    Add files via upload

---
+++ euler.py
@@ -0,0 +1,53 @@
+import numpy as np
+from scipy import signal
+
+def wyznacz_odpowiedz(num,den,czas_trwania_symulacji,czas_trwania_probki,pobudzenie,czestotliwosc,zakres):
+
+    # w tym miejscu mamy gotowy licznik i mianownik naszej finalnej transmitancji dla ktorej musimy eulerem obliczyc odpowiedz
+
+    N = int(czas_trwania_symulacji/czas_trwania_probki)
+    Ts = czas_trwania_probki
+    t = np.arange(N) * Ts
+    u = np.zeros(N)
+
+    
+    if pobudzenie == 1:  #pobudzenie sinusoidalne
+        u = np.sin(2 * np.pi * czestotliwosc * t) 
+        u[(t < zakres[0]) | (t > zakres[1])] = 0
+    elif pobudzenie == 2:  #pobudzenie trojkatne
+        u = signal.sawtooth(2 * np.pi * czestotliwosc * t, width=0.5)
+        u[(t < zakres[0]) | (t > zakres[1])] = 0
+    elif pobudzenie == 3:  #pobudzenie prostokatne o sk. czasie trwania
+        u = (signal.square(2 * np.pi * czestotliwosc * t) + 1) / 2
+        u[(t < zakres[0]) | (t > zakres[1])] = 0
+
+
+    y = np.zeros(N)
+    dy = np.zeros(N)
+    d2y = np.zeros(N)
+    d3y = np.zeros(N)
+    d4y = np.zeros(N)
+
+    du = np.zeros(N)
+    d2u = np.zeros(N)
+    d3u = np.zeros(N)
+
+    for k in range(1, N):
+        du[k] = (u[k] - u[k-1]) / Ts
+    for k in range(2, N):
+        d2u[k] = (du[k] - du[k-1]) / Ts
+    for k in range(3, N):
+        d3u[k] = (d2u[k] - d2u[k-1]) / Ts
+
+
+
+    for k in range(4, N):
+        d4y[k] = (1 / den[0]) * (
+            num[0]*d3u[k] + num[1]*d2u[k] + num[2]*du[k] + num[3]*u[k] - den[1]*d3y[k-1] - den[2]*d2y[k-1] - den[3]*dy[k-1] - den[4]*y[k-1]
+        )
+        d3y[k] = d3y[k-1] + Ts * d4y[k]
+        d2y[k] = d2y[k-1] + Ts * d3y[k]
+        dy[k] = dy[k-1] + Ts * d2y[k]
+        y[k] = y[k-1] + Ts * dy[k]
+
+    return y,u,t
---
+++ menu.py
@@ -0,0 +1,182 @@
+from euler import wyznacz_odpowiedz
+import control as ctrl
+import matplotlib.pyplot as plt
+import numpy as np
+
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+#funkcja wywolujaca odczytanie zmiennych i uruchomienie funkcji wyznaczajacej odpowiedz
+def funkcja():
+
+    a1 = float(entrya1.get())
+    a0 = float(entrya0.get())
+    b2 = float(entryb2.get())
+    b1 = float(entryb1.get())
+    b0 = float(entryb0.get())
+
+    c2 = float(entryc2.get())
+    c1 = float(entryc1.get())
+    c0 = float(entryc0.get())
+    d2 = float(entryd2.get())
+    d1 = float(entryd1.get())
+    d0 = float(entryd0.get())
+
+    l3 = a1 * c2
+    l2 = a1 * c1 + a0 * c2
+    l1 = a1 * c0 + a0 * c1
+    l0 = a0 * c0
+    licznik = [l3, l2, l1, l0]
+
+    d4 = b2 * d2
+    d3 = b2 * d1 + b1 * d2 + a1 * c2
+    d2 = b2 * d0 + b1 * d1 + b0 * d2 + a1 * c1 + a0 * c2
+    d1 = b1 * d0 + b0 * d1 + a1 * c0 + a0 * c1
+    d0 = b0 * d0 + a0 * c0
+    mianownik = [d4, d3, d2, d1, d0]
+
+    zakres = float(entryzakres0.get()), float(entryzakres1.get())
+
+    y,u,t = wyznacz_odpowiedz(licznik,mianownik,float(entryczastrwania.get()),float(entryczasprobki.get()),pobudzenie.get(),float(entryczestotliwosc.get()),zakres)
+
+    fig = Figure(figsize=(6, 4), dpi=100)
+    ax = fig.add_subplot(111)
+    ax.plot(t, u, label='Pobudzenie u(t)', linestyle='--')
+    ax.plot(t, y, label='Odpowiedź y(t)')
+    ax.set_xlabel('Czas [s]')
+    ax.set_ylabel('Wyjście')
+    ax.set_title('Odpowiedź układu – metoda Eulera')
+    ax.grid()
+    ax.legend()
+    canvas_matplotlib = FigureCanvasTkAgg(fig, master=root)
+    canvas_matplotlib.draw()
+    canvas_matplotlib.get_tk_widget().place(relx=0.5, rely=0.75, anchor="center")
+
+    podmianka_okno = """
+    plt.clf()
+    plt.plot(t, u, label='Pobudzenie u(t)', linestyle='--')
+    plt.plot(t, y, label='Odpowiedź y(t)')
+    plt.xlabel('Czas [s]')
+    plt.ylabel('Wyjście')
+    plt.title('Odpowiedź układu – metoda Eulera')
+    plt.grid()
+    plt.legend()
+    plt.show()
+    """
+
+
+#implementacja okna i canvas
+root = Tk()
+root.title("Symulator odpowiedzi ukladu")
+root.resizable(False, False)
+canvas = Canvas(root, width = 800, height = 950, bg = "light gray")
+canvas.pack()
+
+
+#Wybor ksztaltu pobudzenia
+pobudzenie = IntVar()
+
+rb1 = Radiobutton(root, text="Sinus", variable=pobudzenie, value=1)
+rb2 = Radiobutton(root, text="Trojkat", variable=pobudzenie, value=2)
+rb3 = Radiobutton(root, text="Prostokat", variable=pobudzenie, value=3)
+
+canvas.create_window(70, 400, window=rb1)
+canvas.create_window(74, 430, window=rb2)
+canvas.create_window(81, 460, window=rb3)
+
+
+
+#teksty
+canvas.create_text(590, 65, text="Transmitancja obiektu", font=("Arial", 14), fill="black")
+canvas.create_text(330, 65, text="Transmitancja sterownika", font=("Arial", 14), fill="black")
+canvas.create_text(100, 360, text="Kształt pobudzenia", font=("Arial", 14), fill="black")
+canvas.create_text(280, 360, text="Czas pobudzenia", font=("Arial", 14), fill="black")
+canvas.create_text(278, 430, text="Częstotliwość", font=("Arial", 14), fill="black")
+canvas.create_text(500, 360, text="Czas trwania symulacji", font=("Arial", 14), fill="black")
+canvas.create_text(500, 430, text="Długość jednej próbki", font=("Arial", 14), fill="black")
+canvas.create_text(527, 395, text="s", font=("Arial", 14), fill="black")
+canvas.create_text(527, 465, text="s", font=("Arial", 14), fill="black")
+canvas.create_text(308, 466, text="Hz", font=("Arial", 14), fill="black")
+canvas.create_text(280, 394, text="[         ,         ]s", font=("Arial", 14), fill="black")
+
+
+#wejscia transmitancji
+#obiekt
+#licznik
+entrya1 = Entry(root, width = 5)
+canvas.create_window(550,155, window=entrya1)
+entrya0 = Entry(root, width = 5)
+canvas.create_window(625,155, window=entrya0)
+#mianownik
+entryb2 = Entry(root, width = 5)
+canvas.create_window(517,209, window=entryb2)
+entryb1 = Entry(root, width = 5)
+canvas.create_window(598,209, window=entryb1)
+entryb0 = Entry(root, width = 5)
+canvas.create_window(672,209, window=entryb0)
+
+#sterownik
+#licznik
+entryc2 = Entry(root, width = 5)
+canvas.create_window(252,154, window=entryc2)
+entryc1 = Entry(root, width = 5)
+canvas.create_window(335,154, window=entryc1)
+entryc0 = Entry(root, width = 5)
+canvas.create_window(408,154, window=entryc0)
+#mianownik
+entryd2 = Entry(root, width = 5)
+canvas.create_window(252,208, window=entryd2)
+entryd1 = Entry(root, width = 5)
+canvas.create_window(335,208, window=entryd1)
+entryd0 = Entry(root, width = 5)
+canvas.create_window(408,208, window=entryd0)
+
+
+#entry dla parametrow sygnalu pobudzajacego 
+entryzakres0 = Entry(root, width = 5)
+canvas.create_window(250,395, window=entryzakres0)
+
+entryzakres1 = Entry(root, width = 5)
+canvas.create_window(300,395, window=entryzakres1)
+
+entryczestotliwosc = Entry(root, width = 5)
+canvas.create_window(275,465, window=entryczestotliwosc)
+
+
+#entry dla czasu (czas trwania symulacji oraz czas trwania próbki)
+
+entryczastrwania = Entry(root, width = 5)
+canvas.create_window(500,395, window=entryczastrwania)
+
+entryczasprobki = Entry(root, width = 5)
+canvas.create_window(500,465, window=entryczasprobki)
+
+
+#entry - default values
+
+entryzakres0.insert(0, "0")
+entryzakres1.insert(0, "0")
+
+entryczestotliwosc.insert(0,"1")
+entryczasprobki.insert(0,"0.01")
+entryczastrwania.insert(0,"10")
+
+
+# tlo - uklad
+
+img = Image.open("uklad.png")
+img = img.resize((777, 246))
+photo = ImageTk.PhotoImage(img)
+canvas.create_image(400,200,anchor = "center", image=photo)
+
+
+#button - start
+
+przycisk = Button(root, text="Start symulacji", command=funkcja, bg="lightblue", fg="black", font=("Arial", 14, "bold"))
+
+canvas.create_window(700, 420, window=przycisk, width=150, height=100)
+
+root.mainloop()
